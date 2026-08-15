69569a613a4dfb043710d6fc93187ce35583f397
Hakim <hakim@example.com>
2026-08-15 14:17:15 +0200

    feat: initialize native Android Capacitor project for Google Play Store

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evtn.app',
+  appName: 'EVTN',
+  webDir: 'dist'
+};
+
+export default config;
